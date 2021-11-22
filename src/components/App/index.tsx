13a1099eca76b51658f90773d4e3e7c884662005
Harit Joshi <joshiharit3@gmail.com>
2021-11-22 21:43:15 +0530

    initial settings

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.scss";
+
+export class App extends React.Component {
+    render() : JSX.Element {
+        return (
+            <div className= "App">
+                Hello world
+            </div>
+        )
+    }
+}
